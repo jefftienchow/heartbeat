41878d0ffe0251328151515e9873550b3ff7001e
Jeff Chow <Jeff@31-37-141.wireless.csail.mit.edu>
2017-09-05 15:33:07 -0400

    filter

--- cwt_detector.m
+++ cwt_detector.m
@@ -1,16 +1,20 @@
+%created by Jeff Chow to detect heartrates and heartrate variability from
+%vibration sensors
 close all
 clear
-load adapted_wavelet.mat 
 [beat,Fs] = audioread('clear_heart_rate.wav');
 [tmp,Fs] = audioread('single_beat.wav');
-%{
-d = fdesign.lowpass('Fp,Fst,Ap,Ast',0.01,0.02,1,60);
-Hd = design(d,'equiripple');
-ft_beat = filter(Hd,beat);
 
-ft_tmp = filter(Hd,tmp);
-%}
-[wt,f] = cwt(beat, Y, 44100); 
+Hd = fdesign.lowpass('Fp,Fst,Ap,Ast',200,...
+    400,1,60,Fs); %lowpass filter w/ passband frequency 200Hz & stopband frequency 300Hz
+d = design(Hd,'equiripple'); % equiripple filter
+ft_beat = filter(d,beat);
+ft_tmp = filter(d,tmp);
+
+rs_beat = resample(ft_beat, 1, 10); %resample file at 1/10 sampling rate
+rs_tmp = resample(ft_tmp, 1, 10);
+
+[wt,f] = cwt(rs_beat, rs_tmp, 4410); 
 [~,idx125] = min(abs(f-125));
 csf = wt(idx125,:);
 plot(abs(csf))
